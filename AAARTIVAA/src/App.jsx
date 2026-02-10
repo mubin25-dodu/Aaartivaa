788eaccf4d0b9397333714c1e52ec0be4e631260
mubin25-dodu <mubin9516@gmail.com>
2026-02-10 19:24:44 +0600

    1

---
+++ AAARTIVAA/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import Home from "./components/Home";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ AAARTIVAA/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Products() {
+  return (
+    <>
+   <div
+  className="hero min-h-screen mt-16"
+  style={{
+    backgroundImage:
+      "url(https://images.ctfassets.net/qlglka2m35ip/5IMIUOyje3CS9cXkLWsjUd/1ee566765507f2fd05589851a78a49d5/25_DECWK3_US_RENT_SEASONAL_UNAUTH_2UP_A.jpg?q=80&fm=webp)",
+  }}
+>
+  <div className="hero-overlay"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <p className="uppercase tracking-widest text-sm">Clothing and Crafting</p>
+      <h1 className="mb-5 text-5xl font-bold">Wearable Art. Handmade Soul.</h1>
+      <p className="mb-5">
+        Shop artisan-made apparel, textiles, and craft goods designed for everyday wear and gifting.
+        Limited runs, thoughtful details, and materials you can feel.
+      </p>
+      <button className="btn btn-primary">Shop New Arrivals</button>
+    </div>
+  </div>
+</div>
+
+<div className="container ">
+<div></div>
+</div>
+</> 
+ )
+}
