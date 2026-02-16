1ca451111903852765961e0b5ecc43f288ea6494
mubin25-dodu <mubin9516@gmail.com>
2026-02-16 12:29:50 +0600

    feat: initialize React project with Vite, Tailwind CSS, and DaisyUI
    - Added package.json with project metadata and dependencies.
    - Created Vite configuration for React and Tailwind CSS.
    - Implemented main application structure with App component.
    - Added Header and Footer components for navigation and branding.
    - Created Home component with a hero section showcasing products.
    - Included CSS styles for layout and animations.
    - Added SVG assets for branding and icons.
    - Configured ESLint for code quality checks.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/header';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      {/* <Home /> */}
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+    <nav className="navbar bg-base-100 fixed top-0 z-10">
+      <h1 className='text-black'>Aaartivaa</h1>
+    </nav>
+    </>
+  )
+}
